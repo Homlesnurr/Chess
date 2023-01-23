2b383704e9c888938ea06a6109cbe3570adece2c
Homlesnurr <abel.yttervik@gmail.com>
2023-01-24 00:20:01 +0100

    testing

--- Chess game.py
+++ Chess game.py
@@ -3,7 +3,6 @@ import sys
 
 from Pieces import *
 from HasPiece import *
-from ValidMoves import *
 
 # Initialize pygame
 pygame.init()
@@ -80,12 +79,69 @@ def updateState():
 active_piece = None
 turn = 'White'
 valid_moves = []
+attacking_moves = []
+holding = False
 sfx1 = pygame.mixer.Sound('sfx/sfx1.ogg')
+sfx1.set_volume(0.2)
+using_pawn = False
 
 while True:
     for event in pygame.event.get():
+        
         # Check for mouse clicks
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and valid_moves:
+            # Get the mouse position
+            mouse_pos = event.pos
+            
+            # Change the x/y screen coordinates to grid coordinates
+            column = mouse_pos[0] // (width // 8)
+            row = mouse_pos[1] // (height // 8)
+            
+            if [column, row] in valid_moves and [column, row] not in attacking_moves:
+                updateState()
+                active_piece.lastPlacedX = column
+                active_piece.lastPlacedY = row
+                active_piece.x = column
+                active_piece.y = row
+                active_piece = None
+                turn = 'White' if turn == 'Black' else 'Black'
+                valid_moves, attacking_moves = [], []
+            
+            elif [column, row] not in valid_moves and [column, row] not in attacking_moves and has_piece(column, row, pieces, turn) is None:
+                active_piece = None
+                valid_moves, attacking_moves = [], []
+            else:
+                # makes active_piece if it is a piece, and if the piece is the correct color
+                piece_select = has_piece(column, row, pieces, 'nuthin')
+                
+                # Make it so you're holding the piece (reset when you release mouse1)
+                holding = True if piece_select else False
+                
+                # Changes active piece if you click on a different piece and it is the correct color, otherwise it stays the same
+                if active_piece:
+                    active_piece = piece_select if piece_select is not None and piece_select.color == turn else active_piece
+                else:
+                    active_piece = piece_select if piece_select is not None and piece_select.color == turn else None
+                
+                # Changes using_pawn to true if the active piece is a pawn
+                if active_piece.__class__ == Pawn:
+                    using_pawn = True
+                else:
+                    using_pawn = False
+                
+                # Reveal available places piece can move 
+                if active_piece is not None and active_piece.validMoves(pieces):
+                    valid_moves, attacking_moves = active_piece.validMoves(pieces) 
+                elif active_piece is not None and not active_piece.validMoves(pieces):
+                    valid_moves, attacking_moves = [], []
+                else:
+                    valid_moves, attacking_moves
+        
+        # Check for mouse clicks
+        elif event.type == pygame.MOUSEBUTTONDOWN and not valid_moves:
+            # Sets holding to true so that the piece can be moved
+            holding = True
+            
             # Get the mouse position
             mouse_pos = event.pos
             
@@ -97,12 +153,18 @@ while True:
             piece_select = has_piece(column, row, pieces, 'nuthin')
             active_piece = piece_select if piece_select is not None and piece_select.color == turn else None
             
+            # Changes using_pawn to true if the active piece is a pawn
+            if active_piece.__class__ == Pawn:
+                using_pawn = True
+            else:
+                using_pawn = False
+            
             # Reveal available places piece can move 
-            valid_moves = active_piece.validMoves(pieces) if active_piece is not None and active_piece.validMoves(pieces) else valid_moves
+            valid_moves, attacking_moves = active_piece.validMoves(pieces) if active_piece is not None and active_piece.validMoves(pieces) else valid_moves, attacking_moves
 
 
         # moves the piece when mouse1 is held down
-        elif event.type == pygame.MOUSEMOTION and active_piece is not None:
+        elif event.type == pygame.MOUSEMOTION and active_piece is not None and holding:
             # Finds the exact x/y coordinates 
             mouse_pos = event.pos
             column = mouse_pos[0] / (width / 8)
@@ -114,23 +176,28 @@ while True:
 
         # Places piece on square, and removes the piece already on the square, if it exists AND it is holding a piece
         elif event.type == pygame.MOUSEBUTTONUP and active_piece is not None:
+            # Resets holding
+            holding = False
+            
             # Gets x/y grid position, and makes it "attacked_square"
             mouse_pos = event.pos
             column = mouse_pos[0] // (width // 8)
             row = mouse_pos[1] // (height // 8)
-            if [column, row] not in valid_moves:
+            if [column, row] not in valid_moves and [column, row] not in attacking_moves:
                 active_piece.x = active_piece.lastPlacedX
                 active_piece.y = active_piece.lastPlacedY
             
             else:
-                attacked_square = has_piece(column, row, pieces, active_piece.color) if [column, row] in valid_moves else None
-
-                # Removes attacked_square if theres a piece of the opposite color
-                if attacked_square is not None:
-                    pieces.remove(attacked_square)
-                    pygame.mixer.Sound.play(sfx1)
-
+                # Checks if there is a piece on the square
+                attacked_square = has_piece(column, row, pieces, turn)
+                attacked_Piece = attacked_square if isinstance(attacked_square, Piece) else None
                 
+                # Removes attacked_Piece if theres a piece of the opposite color
+                if attacked_Piece is not None:
+                    pieces.remove(attacked_Piece)
+                    pygame.mixer.Sound.play(sfx1)
+                
+                # Checks if pawn is attacking enpassant
                 elif active_piece.__class__.__name__ == 'Pawn':
                     for piece in pieces:
                         if piece.__class__.__name__ == 'Pawn' and piece.enpassant == True:
@@ -141,8 +208,7 @@ while True:
                             elif turn == 'Black':
                                 if piece.x == column and piece.y == row-1:
                                     pieces.remove(piece)
-                                    pygame.mixer.Sound.play(sfx1)
-
+                                    pygame.mixer.Sound.play(sfx1)                                    
                     
                 # Changes active piece location to where mouse1 was released
                 active_piece.x = column
@@ -154,15 +220,17 @@ while True:
                     updateState()
                     active_piece.lastPlacedX = active_piece.x
                     active_piece.lastPlacedY = active_piece.y
+                    # There is no active_piece anymore, so it is set to None
+                    active_piece = None
                 elif turn == 'Black' and (active_piece.x != active_piece.lastPlacedX or active_piece.y != active_piece.lastPlacedY):
                     turn = 'White'
                     updateState()
                     active_piece.lastPlacedX = active_piece.x
                     active_piece.lastPlacedY = active_piece.y
-                valid_moves = []
+                    # There is no active_piece anymore, so it is set to None
+                    active_piece = None
+                valid_moves, attacking_moves = [], []
                 
-            # There is no active_piece anymore, so it is set to None
-            active_piece = None
 
             
              
@@ -194,9 +262,28 @@ while True:
     
     # Draw valid moves
     try:
+        
         for move in valid_moves:
-            pygame.draw.circle(screen, color='Gray', center=(move[0]*60 + 30, move[1]*60 + 30), radius=10, width=3)
+            # Draws a red circle if there is a piece on the square, and a grey circle if there is not 
+            if has_piece(move[0], move[1], pieces, turn):
+                pygame.draw.circle(screen, color=(255,0,0), center=(move[0]*60 + 30, move[1]*60 + 30), radius=10, width=3)
+                
+            else:
+                pygame.draw.circle(screen, color=(55,55,55), center=(move[0]*60 + 30, move[1]*60 + 30), radius=10, width=3)
+            
+            # Draws a red circle if en passant is possible
+            if turn == 'White': # turn variable is changed, so turn == 'White' means its checking valid move for black
+                # Checks if there is a pawn in the square behind the move, and if the piece being used is a pawn, and if it is en passant
+                if has_piece(move[0], move[1]+1, pieces, turn).__class__.__name__ == 'Pawn' and using_pawn and has_piece(move[0], move[1]+1, pieces, turn).enpassant:
+                    pygame.draw.circle(screen, color=(255,0,0), center=(move[0]*60 + 30, move[1]*60 + 30), radius=10, width=3)
+            
+            elif turn == 'Black': # turn variable is changed, so turn == 'Black' means its checking valid move for white
+                # Checks if there is a pawn in the square behind the move, and if the piece being used is a pawn, and if it is en passant
+                if has_piece(move[0], move[1]-1, pieces, turn).__class__.__name__ == 'Pawn' and using_pawn and has_piece(move[0], move[1]-1, pieces, turn).enpassant:
+                    pygame.draw.circle(screen, color=(255,0,0), center=(move[0]*60 + 30, move[1]*60 + 30), radius=10, width=3)
+        
     except:
         print('no nuthin here cuh')
+        pygame.quit()
     # Update the display
     pygame.display.flip()
